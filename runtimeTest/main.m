52f1bfe52726b9ff133cee07200193952a1dd6c5
heweiqi123 <123610105@qq.com>
2019-12-05 23:11:13 +0800

    编译成功的runtime源码

---
+++ runtimeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  runtimeTest
+//
+//  Created by 贺文杰 on 2019/12/1.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
